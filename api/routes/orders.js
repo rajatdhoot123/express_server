73ded424cd929e776dc48dc6a96851d139af0025
Rajat <rajatdhoot123@gmail.com>
2017-12-31 12:00:38 +0530

    another route order added

---
+++ api/routes/orders.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const router = express.Router();
+
+router.get('/',(req,res,next) => {
+	res.status(200).json({
+		message: 'Handling GET request to /orders'
+	})
+})
+
+router.post('/',(req,res,next) => {
+	res.status(200).json({
+		message: 'Handling POST request to /orders'
+	})
+})
+
+router.get('/:orderId',(req,res,next) => {
+	const id = req.params.orderId;
+	if (id === 'special') {
+		res.status(200).json({
+			message: 'This is the request Order',
+			id: id
+		})	
+	}
+})
+
+
+
+router.delete('/:orderId',(req,res,next) => {
+	const orderId = req.params.orderId;
+	res.status(200).json({
+		message: `${orderId} Order Deleted`
+	})
+})
+
+module.exports = router;
